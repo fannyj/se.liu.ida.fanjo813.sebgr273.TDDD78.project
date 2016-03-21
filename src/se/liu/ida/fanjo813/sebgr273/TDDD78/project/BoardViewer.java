a8b1e1578f0fee41f5a1f9f1eb20b8b01c152ea8
fannyj <fanny.jonasson@hotmail.com>
2016-03-21 14:48:34 +0100

    La till menubar

--- src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/BoardViewer.java
+++ src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/BoardViewer.java
@@ -2,6 +2,8 @@ package se.liu.ida.fanjo813.sebgr273.TDDD78.project;
 
 import javax.imageio.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.*;
 import java.io.*;
 import java.awt.Graphics2D;
@@ -12,7 +14,7 @@ public class BoardViewer{
     public static final int HEIGHT = 720;
     private static BufferedImage image;
 
-    public static void main(String[] args) {
+    public BoardViewer(){
         image = null;
         try {
             image = ImageIO.read(ClassLoader.getSystemResource("resources/images/bg.png"));
@@ -23,9 +25,12 @@ public class BoardViewer{
         JFrame myJFrame = new JFrame("Spelet");
         myJFrame.setContentPane(new BoardComponent(image));
         myJFrame.setSize(WIDTH, HEIGHT);
+	createMenus();
         myJFrame.setVisible(true);
         myJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
+
+
 
     public static BufferedImage resizeImage() {
         int type=0;
@@ -36,4 +41,57 @@ public class BoardViewer{
         g.dispose();
         return resizedImage;
     }
+
+    private void createMenus(){
+	final JMenuBar menuBar = new JMenuBar();
+
+	final JMenu optionMenu = new JMenu("Options");
+	final JMenuItem newGameItem = new JMenuItem("New Game");
+	final JMenuItem quitItem = new JMenuItem("Main Menu");
+	optionMenu.add(newGameItem);
+	optionMenu.addSeparator();
+	optionMenu.add(quitItem);
+
+/*
+	newGameItem.addActionListener(new ActionListener() {
+	    @Override public void actionPerformed(final ActionEvent e) {
+		Object[] options = {"Yes", "Cancel"};
+		int optionChosen = JOptionPane.showOptionDialog(null,
+								"New Game",
+								"New Game",
+								JOptionPane.YES_NO_OPTION,
+								JOptionPane.QUESTION_MESSAGE,
+								null,
+								options,
+								options[1]);
+			if (optionChosen == JOptionPane.YES_OPTION){
+			    startNewGame();
+			}
+		    }
+		});
+
+*/
+	quitItem.addActionListener(new ActionListener() {
+	    @Override public void actionPerformed(final ActionEvent e) {
+		Object[] options = {"Yes", "Cancel"};
+		int optionChosen = JOptionPane.showOptionDialog(null,
+								"Are you sure?",
+								"Go to main menu",
+								JOptionPane.YES_NO_OPTION,
+								JOptionPane.QUESTION_MESSAGE,
+								null,
+								options,
+								options[1]);
+		if (optionChosen == JOptionPane.YES_OPTION){
+		    System.exit(0);
+		}
+	    }
+	});
+
+	menuBar.add(Box.createHorizontalGlue());
+	menuBar.add(optionMenu);
+	BoardComponent.setJMenuBar(menuBar);
+
+    }
+
 }
