74635654b7ed06bef26db191d3b77dd01e2a8c8c
fannyj <fanny.jonasson@hotmail.com>
2016-03-21 13:50:55 +0100

    Ändrade koden för bakgrundsbilden. Nu kan alla se oavsätt vilken dator man sitter vid.

--- src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/BoardViewer.java
+++ src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/BoardViewer.java
@@ -10,7 +10,9 @@ public class BoardViewer{
     public static void main(String[] args) {
         BufferedImage image = null;
         try {
-            image = ImageIO.read(new File("C:\\Users\\Aids\\TDDD78\\se.liu.ida.fanjo813.sebgr273.TDDD78.project\\src\\resources\\images\\tomten.png"));
+//            image = ImageIO.read(new File("C:\\Users\\Aids\\TDDD78\\se.liu.ida.fanjo813.sebgr273.TDDD78" + "" +
+//                                           ".project\\src\\resources\\images\\tomten.png"));
+	    image = ImageIO.read(ClassLoader.getSystemResource("resources/images/tomten.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
