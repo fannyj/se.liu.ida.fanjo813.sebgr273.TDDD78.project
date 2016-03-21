0cbb89902f86c0c0989b2fd945b2b05d71611eb9
DeusEc <sebastian.grunditz@gmail.com>
2016-03-21 14:38:34 +0100

    Changed background for main window and some minor changes in BoardViewer

--- src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/BoardViewer.java
+++ src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/BoardViewer.java
@@ -3,24 +3,37 @@ package se.liu.ida.fanjo813.sebgr273.TDDD78.project;
 import javax.imageio.*;
 import javax.swing.*;
 import java.awt.image.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.awt.Graphics2D;
 
 public class BoardViewer{
+
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    private static BufferedImage image;
+
     public static void main(String[] args) {
-        BufferedImage image = null;
+        image = null;
         try {
-//            image = ImageIO.read(new File("C:\\Users\\Aids\\TDDD78\\se.liu.ida.fanjo813.sebgr273.TDDD78" + "" +
-//                                           ".project\\src\\resources\\images\\tomten.png"));
-	    image = ImageIO.read(ClassLoader.getSystemResource("resources/images/tomten.png"));
+            image = ImageIO.read(ClassLoader.getSystemResource("resources/images/bg.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        image = resizeImage();
         JFrame myJFrame = new JFrame("Spelet");
         myJFrame.setContentPane(new BoardComponent(image));
-        myJFrame.setSize(800, 600);
+        myJFrame.setSize(WIDTH, HEIGHT);
         myJFrame.setVisible(true);
         myJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
+    public static BufferedImage resizeImage() {
+        int type=0;
+        type = image.getType() == 0? BufferedImage.TYPE_INT_ARGB : image.getType();
+        BufferedImage resizedImage = new BufferedImage(WIDTH, HEIGHT,type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(image, 0, 0, WIDTH, HEIGHT, null);
+        g.dispose();
+        return resizedImage;
+    }
 }
