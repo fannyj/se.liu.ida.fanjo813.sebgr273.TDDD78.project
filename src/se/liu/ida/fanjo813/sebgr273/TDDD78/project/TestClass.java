525223ae4c7f7c660803fa2d8b708b3b12050fd6
DeusEc <sebastian.grunditz@gmail.com>
2016-03-21 16:02:53 +0100

    litte pill

--- src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/TestClass.java
+++ src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/TestClass.java
@@ -2,11 +2,8 @@ package se.liu.ida.fanjo813.sebgr273.TDDD78.project;
 
 public class TestClass {
     public static void main(String[] args) {
-        GameEngine gameEngine = new GameEngine(10000);
         Player playah = new Player("Gert");
         System.out.println(playah.toString());
-        System.out.println(playah.hasSsd());
-        System.out.println(playah.hasStack());
-        //BoardViewer board = new BoardViewer();
+        BoardViewer board = new BoardViewer();
     }
 }
