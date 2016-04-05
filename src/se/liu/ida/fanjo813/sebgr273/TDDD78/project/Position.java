c65bbb3c15cded9895be862a2fc24c110c48c1a7
fannyj <fanny.jonasson@hotmail.com>
2016-04-05 19:06:02 +0200

    Lagt ihop master med test

--- src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/Position.java
+++ src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/Position.java
@@ -1,20 +1,20 @@
 package se.liu.ida.fanjo813.sebgr273.TDDD78.project;
 
+import java.awt.*;
 import java.util.ArrayList;
 
-public class Position {
+public class Position extends Point
+{
     private int id;
-    private int xPos; //Pixel position i x-led
-    private int yPos; //Pixel position i y-led
+
     private BrickType brick; //Bara city´s kan ha bricks. Alla andra är EMPTY
     private boolean isCity;
     private String cityName;
 
 
     public Position(int id, int xPos, int yPos, BrickType brick, boolean isCity, String cityName) {
+	super(xPos, yPos);
 	this.id = id;
-	this.xPos = xPos;
-	this.yPos = yPos;
 	this.brick = brick;
 	this.isCity = isCity;
 	this.cityName = cityName;
@@ -23,14 +23,6 @@ public class Position {
 
     public int getId() {
 	return id;
-    }
-
-    public int getxPos() {
-	return xPos;
-    }
-
-    public int getyPos() {
-	return yPos;
     }
 
     public BrickType getBrick() {
