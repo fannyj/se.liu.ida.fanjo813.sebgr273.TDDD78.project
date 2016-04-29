dfe711dc500573f02edf9d829778d8b5e1c0572c
DeusEc <sebastian.grunditz@gmail.com>
2016-04-29 15:28:07 +0200

    Added javadoc for Brick-class

--- src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/Brick.java
+++ src/se/liu/ida/fanjo813/sebgr273/TDDD78/project/Brick.java
@@ -2,6 +2,10 @@ package se.liu.ida.fanjo813.sebgr273.TDDD78.project;
 
 import java.awt.Point;
 
+/**
+ * Handles the creation of a new brick, and the interaction between the rest
+ * of the game and the brick
+ */
 public class Brick {
     private Point boardPosition;
     private BrickType brickType;
